e0e04704d23235d1c909afe022cf3a5a61d7d666
Kgrochowsk <87641938+Kgrochowsk@users.noreply.github.com>
2021-11-08 22:09:17 +0100

    Add files via upload

---
+++ Sorting tool/Main.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class Main {
+    public static void main(final String[] args){
+
+        String dataType = "";
+        String sortType = "natural";
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-sortingType")) {
+                try {
+                    sortType = args[i + 1];
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No sorting type defined!");
+                }
+            } else if (args[i].equals("-dataType")) {
+                try {
+                    dataType = args[i + 1];
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No data type defined!");
+                }
+            }
+        }
+        
+        Console console = new Console();
+        console.OpenConsole(dataType, sortType);
+    }
+}
